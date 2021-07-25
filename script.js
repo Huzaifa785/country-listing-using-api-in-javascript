41d89774ebf4d3a67793baa933e0cea52c1c75a0
Huzaifa Mohammed <huzaifa.coder785@gmail.com>
2021-07-25 22:26:05 +0530

    Updated using the async/await and added the try/catch block.

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-function getCountries() {
-    fetch("https://restcountries.eu/rest/v2/all", {
-        method: "GET"
-    })
-        .then((data) => {
-            return data.json();
-        })
-        .then((countries) => loadCountries(countries));
+let getCountries = async () => {
+    try {
+        let response = await fetch("https://restcountries.eu/rest/v2/all")
+        let countries = await response.json()
+        const countryList = document.createElement("div");
+        countryList.className = "country-list";
+        countries.forEach((country) => {
+        const cardsContainer = document.createElement("div");
+        cardsContainer.className = "cards-container";
+    
+        cardsContainer.innerHTML = `
+                                    <img class="flag-image"  src=${country.flag}> </img>
+                                    <div class="after-flag">
+                                    <h2 class="country-name">${country.name}</h2>
+                                    <p class="population"><b>Population:</b> ${country.population}</p>
+                                    <p class="region"><b>Region:</b> ${country.region}</p>
+                                    <p class="capital"><b>Capital:</b> ${country.capital}</p>
+                                    </div>
+                                   `;
+    
+            countryList.append(cardsContainer);
+
+            document.body.append(countryList);
+        });
+    } catch (error) {
+        countryList.innerHTML = "Oops! Something went wrong...."
+        console.log("Error: ", error);
+    }
 }
 
 getCountries()
-
-function loadCountries(country) {
-        const countryList = document.createElement("div");
-        countryList.className = "country-list";
-        country.forEach((country) => {
-            const cardsContainer = document.createElement("div");
-            cardsContainer.className = "cards-container";
-
-            cardsContainer.innerHTML = `
-    <img class="flag-image"  src=${country.flag}> </img>
-    <div class="after-flag">
-      <h2 class="country-name">${country.name}</h2>
-      <p class="population"><b>Population:</b> ${country.population}</p>
-      <p class="region"><b>Region:</b> ${country.region}</p>
-      <p class="capital"><b>Capital:</b> ${country.capital}</p>
-    </div>
-    `;
-
-            countryList.append(cardsContainer);
-        });
-
-        document.body.append(countryList);
-    }
-
-loadCountries()
